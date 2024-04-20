522a4edb89c60628cb0feb3a05f35947afbcbf93
mtalhaaygen <mtalha.aygen18@gmail.com>
2024-04-20 18:47:54 +0300

    fix

--- srcs/requirements/wordpress/tools/wordpress_start.sh
+++ srcs/requirements/wordpress/tools/wordpress_start.sh
@@ -2,7 +2,7 @@
 
 	sed -i "s/listen = \/run\/php\/php7.3-fpm.sock/listen = 9000/" "/etc/php/7.3/fpm/pool.d/www.conf";
 	chown -R www-data:www-data /var/www/*;
-	chown -R 755 /var/www/*;
+	chmod -R 755 /var/www/*;
 	mkdir -p /run/php/;
 	touch /run/php/php7.3-fpm.pid;
 
